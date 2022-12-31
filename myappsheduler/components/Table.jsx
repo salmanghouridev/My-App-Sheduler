67cece28746db26a588d1c856014792cd9262ada
Salman Ghouri <salmanghoridev@gmail.com>
2022-12-31 18:26:46 +0500

    make UI 10%
    make UI 10%

--- myappsheduler/components/Table.jsx
+++ myappsheduler/components/Table.jsx
@@ -1,49 +1,54 @@
-export default function Table() {
-  return (
-    <>
-      <div className="overflow-x-auto">
-        <table className="min-w-full divide-y divide-gray-200 text-sm">
-          <thead className="bg-gray-100">
-            <tr>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
-                Name
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
-                Date of Birth
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
-                Role
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
-                Salary
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
-                Status
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 bg-green-600">
-                Action
-              </th>
-            </tr>
-          </thead>
+import { BiEdit, BiTrashAlt } from "react-icons/bi";
 
-          <tbody className="divide-y divide-gray-200">
-            <tr>
-              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                John Doe
-              </td>
-              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                24/05/1995
-              </td>
-              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                Web Developer
-              </td>
-              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                $120,000
-              </td>
-            </tr>
-          </tbody>
+export default function Table(){
+    return (
+        <table className="min-w-full table-auto">
+            <thead>
+                <tr className="bg-gray-800">
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Name</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Email</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Salary</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Birthday</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Status</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Actions</span>
+                    </th>
+                </tr>
+            </thead>
+            <tbody className="bg-gray-200">
+                <tr className="bg-gray-50 text-center">
+                    <td className="px-16 py-2 flex flex-row items-center">
+                        <img src="#" alt="" />
+                        <span className="text-center ml-2 font-semibold">Daily Tuition</span>
+                    </td>
+                    <td className="px-16 py-2">
+                        <span>dailytuition@gmail.com</span>
+                    </td>
+                    <td className="px-16 py-2">
+                        <span>$25000</span>
+                    </td>
+                    <td className="px-16 py-2">
+                        <span>10-05-2022</span>
+                    </td>
+                    <td className="px-16 py-2">
+                        <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">Active</span></button>
+                    </td>
+                    <td className="px-16 py-2 flex justify-around gap-5">
+                        <button className="cursor"><BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit></button>
+                        <button className="cursor"><BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt></button>
+                    </td>
+                </tr>
+            </tbody>
         </table>
-      </div>
-    </>
-  );
+    )
 }
